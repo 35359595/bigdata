3e31143a3ddce98eacf51515e0587fc74cf72653
35359595 <35359595i@gmail.com>
2015-10-13 21:36:50 +0300

    uniq funct added!

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ fn main() {
 	let mut y6: Vec<Vec<String>> = vec![];
 	let mut els: Vec<Vec<String>> = vec![];
 	
+	let mut p1: Vec<String> = vec![];
+
 	let mut train = csv::Reader::from_file("./data/train.csv").unwrap();
 	for train_record in train.decode() {
 		let s1: Vec<String> = train_record.unwrap();
@@ -25,5 +27,34 @@ fn main() {
 		else if s1[62] == "6" { &y6.push(s1); }
 		else { &els.push(s1); }
 	}
+
 	println!("0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, else: {}", y0.len(), y1.len(), y2.len(), y3.len(), y4.len(), y5.len(), y6.len(), els.len());
+	
+	p1 = find_uniq(y0, 2);
+	for p in p1 { println!("Uniq in a row {}", p); }
+	
+	//interesting part
+	if els.len() != 0 {panic!("Parse went wrong! els is not epmty!")} //if parse of test was not complete as expected
+	
+	//writing results
+//	let mut writer = csv::Writer::from_file("./data/y0.csv").unwrap();
+//		for y00 in y0 {
+//			writer.encode(y00).ok().expect("CSV Writer error.");
+//		}
+
+//	let mut test = csv::Reader::from_file("./data/test.csv").unwrap(); //loadint work data
+	
+}
+
+fn find_uniq(v: Vec<Vec<String>>, i: usize) -> Vec<String> {
+	let mut ret: Vec<String> = vec![];
+	for var in &v {
+		let mut check = false;
+		let st1: &String = &var[i];
+		for r in &ret {
+			if r == st1 { check = true; }
+		}
+		if ! check { &ret.push(st1.to_string()); }
+	}
+	return ret
 }
