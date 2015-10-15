b02d206b708871ea27557645f7e9ff69d33c7b2a
35359595 <35359595i@gmail.com>
2015-10-15 13:29:45 +0300

    Finaly working!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate csv;
 
+use std::collections::HashSet;
+
 struct All {
 	y0: Vec<Vec<String>>,
 	y1: Vec<Vec<String>>,
@@ -19,6 +21,16 @@ struct Uniq {
 	p4: Vec<String>,
 	p5: Vec<String>,
 	p6: Vec<String>,
+}
+
+struct RCounter {
+	c0: i32,
+	c1: i32,
+	c2: i32,
+	c3: i32,
+	c4: i32,
+	c5: i32,
+	c6: i32,
 }
 
 fn main() {
@@ -44,17 +56,23 @@ fn main() {
 	//All must be mutable to remove matches...
 
 	uniq.p0 = find_uniq(all.y0);
+	println!("15% done..");
 	uniq.p1 = find_uniq(all.y1);
+	println!("30% done..");
 	uniq.p2 = find_uniq(all.y2);
+	println!("45% done..");
 	uniq.p3 = find_uniq(all.y3);
+	println!("60% done..");
 	uniq.p4 = find_uniq(all.y4);
+	println!("75% done..");
 	uniq.p5 = find_uniq(all.y5);
+	println!("90% done..");
 	uniq.p6 = find_uniq(all.y6);
-//	for p in uniq.p0 { println!("Uniq 0: {}", p); }
+	println!("100% uniq search done!\n");
 
 	//interesting part
 	if all.els.len() != 0 {panic!("Parse went wrong! els is not epmty!")} //if parse of test was not complete as expected
-	
+	build_rating(&uniq);
 	//writing results
 //	let mut writer = csv::Writer::from_file("./data/y0.csv").unwrap();
 //		for y00 in y0 {
@@ -66,26 +84,62 @@ fn main() {
 }
 
 fn find_uniq(v: Vec<Vec<String>>) -> Vec<String> {
-	let mut ret: Vec<String> = vec![];
-	for i in 0..62 {
-	for var in &v {
-		let mut check = false;
-		let st1: &String = &var[i];
-		for r in &ret {
-			if r == st1 { check = true; }
-		}
-		if ! check { &ret.push(st1.to_string()); }
-	}
-	}
-	return ret
+	v.into_iter().flat_map(|v| v).collect::<HashSet<String>>().into_iter().collect()
 }
 
-fn find_same(v1: &Vec<String>, v2: &Vec<String>) -> Vec<String> {
-	let mut ret: Vec<String> = vec![];
-	for var1 in v1 {
-		for var2 in v2 {
-			if var1 == var2 { &ret.push(var1.to_string()); }
+fn build_rating(uniq: &Uniq) {
+	println!("building rationgs and printing results:\n");
+	println!("ID, y");
+	let mut test = csv::Reader::from_file("./data/test.csv").unwrap();
+		for test_rows in test.decode() {
+			let mut rate = RCounter {c0: 0,c1: 0,c2: 0,c3: 0,c4: 0,c5: 0,c6: 0};
+			let test_row: Vec<String> = test_rows.unwrap();
+			for test_string in &test_row {
+				for u in &uniq.p0 {
+					if test_string == &u.to_string() {
+						rate.c0 += 1;
+					}
+				}
+				for u in &uniq.p1 {
+					if test_string == &u.to_string() {
+						rate.c1 += 1;
+					}
+				}
+				for u in &uniq.p2 {
+					if test_string == &u.to_string() {
+						rate.c2 += 1;
+					}
+				}
+				for u in &uniq.p3 {
+					if test_string == &u.to_string() {
+						rate.c3 += 1;
+					}
+				}
+				for u in &uniq.p4 {
+					if test_string == &u.to_string() {
+						rate.c4 += 1;
+					}
+				}
+				for u in &uniq.p5 {
+					if test_string == &u.to_string() {
+						rate.c5 += 1;
+					}
+				}
+				for u in &uniq.p6 {
+					if test_string == &u.to_string() {
+						rate.c6 += 1;
+					}
+				}
+			}
+			let mut decision = 0;
+			if rate.c0 > rate.c1 { decision = 0 }
+			else if rate.c1 > rate.c2 { decision = 1 }
+			else if rate.c2 > rate.c3 { decision = 2 }
+			else if rate.c3 > rate.c4 { decision = 3 }
+			else if rate.c4 > rate.c5 { decision = 4 }
+			else if rate.c5 > rate.c6 { decision = 5 }
+			else { decision = 6 }
+			let sting: &String = &test_row[0].to_string();
+			println!("{}, {}", sting, decision);
 		}
-	}
-	return ret
-}	
+}
